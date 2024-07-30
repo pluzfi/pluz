89a85400fabc21c16c27e930cb7b3d37caab63db
pluz <pluz.fi2024@gmail.com>
2024-07-30 17:06:38 +0800

    init

--- contracts/interfaces/IAssetPriceOracle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.24;
-
-/// @notice Interface for a price oracle preconfigured to return the price of an asset.
-/// @dev Price can be in any denomination, depending on the preconfiguration.
-interface IAssetPriceOracle {
-    function getPrice() external view returns (uint256 price);
-}
